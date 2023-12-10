86a9d4fbe265b0e0ea38f52696672fe0d0bcda54
Santiago Cano <65575894+santiagocano22@users.noreply.github.com>
2023-12-09 22:56:34 -0500

    Delete invalid_argument_error.py

--- invalid_argument_error.py
+++
@@ -1,9 +0,0 @@
-# ---------------------------------------------------------------------------------------------
-# PLEASE DO NOT MODIFY, RENAME OR REMOVE ANY OF CLASSES, METHODS AND VARIABLES BELOW.
-# YOU CAN ADD YOUR OWN METHODS AND VARIABLES TO THE EXISTING CLASSES AND USE THEM IN YOUR WORK.
-# ---------------------------------------------------------------------------------------------
-
-
-class InvalidArgumentError(RuntimeError):
-    # You can add your own class members here.
-    pass
